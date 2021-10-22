afffc251baa8c716b72d2a20caa41e9c533aa4a2
p22lmendizabal <p22lmendizabal@fairfieldprep.org>
2021-10-22 09:59:07 -0400

    hw 5-1 not done

---
+++ hw5-1.py
@@ -0,0 +1,16 @@
+# author: LM (AMDG) 10/22/21
+import random
+# Question 1
+print(random.randrange(31, 50))
+# Question 2
+print(random.randrange(1, 75, 2))
+# Question 3
+print(random.randint(20, 30))
+# Question 4
+print(random.randrange(1, 11, 8))
+# Question 5
+print(random.randrange(1, 11, 20))
+# Question 6
+print(random.choices['cat', 'dog', 'sheep', 'cow', 'chicken', 'pig', 1])
+# Question 7
+print(random.choices([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1))
