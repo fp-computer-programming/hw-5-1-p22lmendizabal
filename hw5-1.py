c541939737a65e29663da2c87f6db02edc3dd717
p22lmendizabal <p22lmendizabal@fairfieldprep.org>
2021-10-26 10:21:11 -0400

    5-1

--- hw5-1.py
+++ hw5-1.py
@@ -3,14 +3,15 @@ import random
 # Question 1
 print(random.randrange(31, 50))
 # Question 2
-print(random.randrange(1, 75, 2))
+print(random.randrange(2, 75, 2))
 # Question 3
 print(random.randint(20, 30))
 # Question 4
-print(random.randrange(1, 11, 8))
+print(random.randrange(1, 13))
 # Question 5
-print(random.randrange(1, 11, 20))
+print(random.randrange(1, 21))
 # Question 6
 print(random.choices['cat', 'dog', 'sheep', 'cow', 'chicken', 'pig', 1])
 # Question 7
 print(random.choices([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1))
+# Question 8
