5fe39fc4056f196b0672694ab3f5a99f4f957767
p22mendizabal <p22lmendizabal@fairfieldprep.org>
2021-10-30 20:58:00 -0400

    add file

--- hw5-1.py
+++ hw5-1.py
@@ -11,9 +11,15 @@ print(random.randrange(1, 13))
 # Question 5
 print(random.randrange(1, 21))
 # Question 6
-mylist = ['cat', 'dog', 'sheep', 'cow', 'chicken', 'pig']
-print(random.choice(mylist))
+print(random.choice(['cat', 'dog', 'sheep', 'cow', 'chicken', 'pig']))
 # Question 7
-list2 = ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10],1)
-print(random.choices(list2))
+print(random.choices([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], k=1))
 # Question 8
+print(random.choices([1, 2, 3, 4, 5], k=5))
+# question 9
+cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51,52] 
+random.shuffle(cards)
+print(cards)
+# question 10
+random.seed(1942)
+print(random.randrange(1, 1001))
